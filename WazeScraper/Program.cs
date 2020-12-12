33bec1029f98452c0e23e69313efb86f1c493d98
Amandas Slabšys <amandas.slabsys@gmail.com>
2020-12-12 18:22:06 +0200

    Add project files.

---
+++ WazeScraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WazeScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initial commit");
+        }
+    }
+}
