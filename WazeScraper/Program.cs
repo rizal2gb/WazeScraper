20878e76265a867be432f0255d39ececb26bf841
Amandas Slabšys <amandas.slabsys@gmail.com>
2021-11-03 20:40:04 +0200

    WIP logging

--- WazeScraper/Program.cs
+++ WazeScraper/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,12 +10,15 @@ namespace WazeScraper
     class Program
     {
         private static readonly ManualResetEvent QuitEvent = new ManualResetEvent(false);
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
 
         static async Task Main()
         {
             try
             {
-                Console.WriteLine("Starting...");
+                SetupLogger();
+                Logger.Info("Starting...");
                 AutofacInitializer.Initialize();
                 var scraper = AutofacInitializer.GetInstance<Scraper>();
                 await scraper.Start();
@@ -27,8 +33,20 @@ namespace WazeScraper
             }
             catch (Exception e)
             {
-                Console.WriteLine($"Application has crashed, Exception: {e.Message}\n StackTrace: \n {e.StackTrace}");
+                Logger.Info($"Application has crashed, Exception: {e.Message}\n StackTrace: \n {e.StackTrace}");
             }
+        }
+
+        private static void SetupLogger()
+        {
+            var config = new LoggingConfiguration();
+            var consoleTarget = new FileTarget
+            {
+                FileName = "Logs-${shortdate}.log",
+                Layout = "${longdate} ${level:uppercase=true} ${message} ${exception:format=tostring}"
+            };
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, consoleTarget, "*");
+            LogManager.Configuration = config;
         }
     }
 }
